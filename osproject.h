5e0e034d1da0e567a2ad4e49086761eb013a4026
Upamanyu Ghose <upamanyughose@Upamanyus-MacBook-Pro.local>
2017-10-18 14:07:44 +0530

    Added the header file with basic function names

---
+++ osproject.h
@@ -0,0 +1,13 @@
+/*
+	I have just included the funcion names. Please note any changes needed as the return type and parameters. 
+	Do not make the change, just remember it and we'll make the change later. 
+*/
+
+#ifndef OS_Project
+#define OS_Project
+
+void compileCode();
+void downloadFile();
+void uploadFile();
+
+#endif
